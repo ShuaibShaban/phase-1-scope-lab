583f02c1d410e980bfea41a3591fdb5920251380
shuaib shaban <shuaib>
2022-11-27 22:18:01 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+console.log(customerName);
+
+const leastFavoriteCustomer = 'shaban'
+
+function upperCaseCustomerName()
+{
+    customerName = 'bob'.toUpperCase();
+
+}
+function setBestCustomer()
+{
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer()
+{
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer()
+{
+     leastFavoriteCustomer = 'shaban';
+}
